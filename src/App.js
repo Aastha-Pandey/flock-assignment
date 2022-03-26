0126cbe8e710913f28744a30754881c927c4d6f5
Aastha Pandey <aasthap7@gmail.com>
2022-03-26 02:44:13 -0800

    add ga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './index.css';
 import Main from './components/Layout/Main';
-
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-7GPQ1E859J');
+ReactGA.pageview(window.location.pathname + window.location.search);
 function App() {
   return <Main />;
 }
